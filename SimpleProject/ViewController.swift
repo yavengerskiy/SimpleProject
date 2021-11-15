7485b00895af0afa9d21ce4e8981fb016dac4398
YaYuVengerskiy <yavengerskiy@yandex.ru>
2021-11-15 21:28:20 +0500

    Initial Commit

---
+++ SimpleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleProject
+//
+//  Created by Beelab on 15/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
